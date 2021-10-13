08533651df942578deaa56c0ef71ddc6faacad4c
Dev4 <Dev4@root.maysam.com>
2021-10-13 16:03:01 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,150 @@
+
+
+function send(e) {
+    let name = document.getElementById("name").value;
+    let position = document.getElementById("position").value;
+    let loc = document.getElementById("loc").value;
+    let education = document.getElementById("education").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let about = document.getElementById("about").value;
+    let right = document.querySelector(".right");
+    let res = document.querySelector(".info-res");
+
+    let formArr = {name,position,loc,education,phone,email,about};
+    // console.log(formArr)
+    // console.log(Object.keys(formArr))
+    // formArr.push(name,position,loc,education,phone,email,about)
+
+    res.classList.add("active");
+
+      Object.keys(formArr).forEach((elem) =>{
+      let p = document.createElement("p");
+      const newContent = document.createTextNode(elem + " : " +formArr[elem]);
+      p.appendChild(newContent);
+      res.appendChild(p);
+  })
+}
+
+function closeInfo (e) {
+  let canselBtn = document.querySelector(".cancel-btn");
+  let res = document.querySelector(".info-res");
+  res.classList.remove("active");
+}
+
+let addBtn = document.querySelector(".add-btn");
+addBtn.addEventListener('click', ()=> {
+
+})
+
+let addButton = document.querySelector(".add-btn");
+addBtn.addEventListener('click' , () => {
+  callModal();
+})
+
+let closeButton = document.querySelector(".close-btn");
+closeButton.addEventListener('click' , () =>{
+  let backDrop = document.querySelector(".backdrop");
+  backDrop.classList.remove("active");
+});
+
+let backDrop = document.querySelector(".backdrop");
+
+backDrop.addEventListener('click' , ()=>{
+  let backDrop = document.querySelector(".backdrop");
+  backDrop.classList.remove("active");
+});
+
+// let cancelButton = document.querySelector(".cancel-btn");
+// cancelButton.addEventListener('click', ()=>{
+//   let backDrop = document.querySelector(".backdrop");
+//   backDrop.classList.remove("active");
+// })
+
+function cancelModal(e) {
+
+
+  document.querySelector(".backdrop");
+  backDrop.classList.remove("active");
+}
+
+///////////////////////////////////
+
+
+
+
+
+let socialArr = [];
+
+function callModal(id){
+  document.getElementById("url").value = '';
+  document.getElementById("icon").value = '';
+  document.getElementById("description").value = '';
+
+  let backDrop = document.querySelector(".backdrop");
+  backDrop.classList.add("active");
+
+  document.querySelector(".save-btn").setAttribute("onclick", "saveInfo("+id+")")
+}
+
+
+function saveInfo(id) {
+  
+  console.log(id)
+  
+
+  let urlInput = document.getElementById("url").value;
+  let iconInput = document.getElementById("icon").value;
+  let descInput = document.getElementById("description").value;
+
+  if(id===undefined){
+    
+    let socialObj = {urlInput,iconInput,descInput, id: socialArr.length}
+    socialArr.push(socialObj);
+  }else{
+    let data = socialArr.filter(elem=>elem.id == id)[0];
+    data.urlInput = urlInput
+    data.iconInput = iconInput
+    data.descInput = descInput
+  }
+  
+  let backDrop = document.querySelector(".backdrop");
+  backDrop.classList.remove("active");
+
+  printSoc();
+
+}
+
+function printSoc(){
+  let myLi = socialArr.reduce((total, elem)=>{
+    return total+= `<li>` + elem.descInput + `<button onclick="editSoc(${elem.id})">Edit</button><button onclick="deleteSoc(${elem.id})">Delete</button></li>`
+  }, '');
+  document.querySelector('.info-div').innerHTML = '<ul>' + myLi + '</ul>';
+}
+
+function deleteSoc(id){
+  socialArr = socialArr.filter(elem=>elem.id!==id);
+  printSoc();
+}
+
+function editSoc(id){
+  //open modal
+  callModal(id)
+  let data = socialArr.filter(elem=>elem.id == id)[0]
+  //input value set
+  document.getElementById("url").value = data.urlInput;
+  document.getElementById("icon").value = data.iconInput;
+  document.getElementById("description").value = data.descInput;
+
+}
+
+// function editObj(id){
+//   let urlInput = document.getElementById("url").value;
+//   let iconInput = document.getElementById("icon").value;
+//   let descInput = document.getElementById("description").value;
+
+//   let data = socialArr.filter(elem=>elem.id == id)[0];
+//   data.urlInput = urlInput
+//   data.iconInput = iconInput
+//   data.descInput = descInput
+// }
